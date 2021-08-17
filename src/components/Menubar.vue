4dddb4636762a22345f1f41a5943e6daf56603cf
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-08-18 07:57:59 +0900

    add new components

---
+++ src/components/Menubar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="menubar">
+    <h1>Tagging Box</h1>
+    <label>
+      <input type="file" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menubar",
+};
+</script>
+
+<style>
+label > input {
+  display: none;
+}
+label {
+  padding: 0 1rem;
+  border: solid 1px #888;
+}
+label::after {
+  content: "Upload";
+  font-size: 1rem;
+  color: #666;
+  padding-left: 1rem;
+}
+</style>
