077cc0980f207a6c42a108889ff4720aa95ee57e
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-08-21 07:32:59 +0900

    output tagged data method

--- src/components/OutputData.vue
+++ src/components/OutputData.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="output-data">
-    <pulldown-menu>
+    <pulldown-menu ref="menu">
       <pull-down-menu-list v-on:list-clicked="outputAsAppData">
-        出力
+        このアプリの形式で出力
       </pull-down-menu-list>
       <pull-down-menu-list v-on:list-clicked="outputAsTaggedData">
-        出力
+        タグ付けモードで出力
       </pull-down-menu-list>
     </pulldown-menu>
   </div>
@@ -29,12 +29,35 @@ export default {
         ],
         { type: "text/plain" }
       );
+      this.downloadFile(contents);
+    },
+    outputAsTaggedData() {
+      alert(
+        "ここで書き出されたファイルは再読み込みできません。再度作業をする際は必ずこのアプリの形式で保存してください。"
+      );
+      const itemArray = [];
+      console.log(this.$store.state.itemData);
+      for (let item of this.$store.state.itemData) {
+        const itemData = new Object({ name: item.name, tags: [] });
+        for (let box of this.$store.state.boxData.slice(1)) {
+          if (box.contents.indexOf(item.itemId) !== -1) {
+            itemData.tags.push(box.boxName);
+          }
+        }
+        itemArray.push(itemData);
+      }
+      const contents = new Blob([JSON.stringify(itemArray)], {
+        type: "text/plain",
+      });
+      this.downloadFile(contents);
+    },
+    downloadFile(contents) {
       const link = document.createElement("a");
       link.href = window.URL.createObjectURL(contents);
       link.download = this.$store.state.fileName;
       link.click();
+      this.$refs.menu.close();
     },
-    outputAsTaggedData() {},
   },
 };
 </script>
